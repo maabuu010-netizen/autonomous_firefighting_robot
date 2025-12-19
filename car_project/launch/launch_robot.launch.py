92b148c003e1a205ce3065b0c177de1c7bf46a1e
Huy <maabuu010@gmail.com>
2025-12-20 01:19:47 +0700

    up

--- car_project/launch/launch_robot.launch.py
+++ car_project/launch/launch_robot.launch.py
@@ -74,7 +74,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
